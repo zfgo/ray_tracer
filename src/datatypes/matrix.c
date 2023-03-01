c138208facc2abf06de6cab46e229b472aea4e7a
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-01 10:12:43 -0800

    update matrix functions after changing vector structure

--- src/datatypes/matrix.c
+++ src/datatypes/matrix.c
@@ -1,7 +1,7 @@
 #include "datatypes/matrix.h"
 #include "include.h"
 
-bool matrix4_equal(Matrix4 m1, Matrix4 m2)
+bool matrix4_equal(Mat4 m1, Mat4 m2)
 {
     int i, j;
 
@@ -16,7 +16,7 @@ bool matrix4_equal(Matrix4 m1, Matrix4 m2)
     return true;
 }
 
-bool matrix3_equal(Matrix3 m1, Matrix3 m2)
+bool matrix3_equal(Mat3 m1, Mat3 m2)
 {
     int i, j;
 
@@ -31,7 +31,7 @@ bool matrix3_equal(Matrix3 m1, Matrix3 m2)
     return true;
 }
 
-bool matrix2_equal(Matrix2 m1, Matrix2 m2)
+bool matrix2_equal(Mat2 m1, Mat2 m2)
 {
     int i, j;
 
@@ -46,9 +46,9 @@ bool matrix2_equal(Matrix2 m1, Matrix2 m2)
     return true;
 }
 
-Matrix4 matrix4_compose(Matrix4 m1, Matrix4 m2)
+Mat4 matrix4_compose(Mat4 m1, Mat4 m2)
 {
-    Matrix4 res;
+    Mat4 res;
     int i, j, k;
 
     for (i = 0; i < 4; ++i) {
@@ -62,37 +62,23 @@ Matrix4 matrix4_compose(Matrix4 m1, Matrix4 m2)
     return res;
 }
 
-Vector matrix4_vector_multipy(Matrix4 m, Vector v)
+Vec matrix4_vector_multipy(Mat4 m, Vec v)
 {
-    Vector res;
-    int i;
+    Vec res;
+    int i, j;
 
-    res.x = v.x * m.A[0][0]
-          + v.y * m.A[1][0]
-          + v.z * m.A[2][0]
-          + v.w * m.A[3][0];
-
-    res.y = v.x * m.A[0][1]
-          + v.y * m.A[1][1]
-          + v.z * m.A[2][1]
-          + v.w * m.A[3][1];
-
-    res.z = v.x * m.A[0][2]
-          + v.y * m.A[1][2]
-          + v.z * m.A[2][2]
-          + v.w * m.A[3][2];
-
-    res.w = v.x * m.A[0][3]
-          + v.y * m.A[1][3]
-          + v.z * m.A[2][3]
-          + v.w * m.A[3][3];
+    for (i = 0; i < 4; ++i) {
+        for (j = 0; j < 4; ++j) {
+            res.v[i] += v.v[j] * m.A[j][i];
+        }
+    }
 
     return res;
 }
 
-Matrix4 matrix4_transpose(Matrix4 m)
+Mat4 matrix4_transpose(Mat4 m)
 {
-    Matrix4 res;
+    Mat4 res;
     int i, j;
 
     for (i = 0; i < 4; ++i) {
@@ -104,41 +90,38 @@ Matrix4 matrix4_transpose(Matrix4 m)
     return res;
 }
 
-float matrix4_determinant(Matrix4 m)
+float matrix4_determinant(Mat4 m)
 {
     return 0.0;
 }
 
-float matrix3_determinant(Matrix3 m)
+float matrix3_determinant(Mat3 m)
 {
     return 0.0;
 }
 
-float matrix2_determinant(Matrix2 m)
+float matrix2_determinant(Mat2 m)
 {
     return m.A[0][0] * m.A[1][1] - m.A[0][1] * m.A[1][0];
 }
 
-Matrix3 matrix4_submatrix(Matrix4 m, int r, int c)
+Mat3 matrix4_submatrix(Mat4 m, int r, int c)
 {
-    Matrix3 res;
+    Mat3 res;
     int i, j;
     int out_i = 0,
         out_j = 0;
 
     for (i = 0; i < 4; ++i) {
-        if (i == r) {
-
+        if (i == r)
             continue;
-        }
         for (j = 0; j < 4; ++j) {
-            if (j == c) {
+            if (j == c)
                 continue;
-            }
             res.A[out_i][out_j] = m.A[i][j];
             ++out_j;
         }
         ++out_i;
     }
-
+    return res;
 }
--- src/datatypes/matrix.h
+++ src/datatypes/matrix.h
@@ -7,60 +7,60 @@
 typedef struct matrix4
 {
     float A[4][4];
-} Matrix4;
+} Mat4;
 
 typedef struct matrix3
 {
     float A[3][3];
-} Matrix3;
+} Mat3;
 
 typedef struct matrix2
 {
     float A[2][2];
-} Matrix2;
+} Mat2;
 
 /* given two 4x4 matrices, determine if they are equal or not
  */
-bool matrix4_equal(Matrix4 m1, Matrix4 m2);
+bool matrix4_equal(Mat4 m1, Mat4 m2);
 
 /* given two 3x3 matrices, determine if they are equal or not
  */
-bool matrix3_equal(Matrix3 m1, Matrix3 m2);
+bool matrix3_equal(Mat3 m1, Mat3 m2);
 
 /* given two 2x2 matrices, determine if they are equal or not
  */
-bool matrix2_equal(Matrix2 m1, Matrix2 m2);
+bool matrix2_equal(Mat2 m1, Mat2 m2);
 
 /* given two 4x4 matrices, multiply them together and return the result
  */
-Matrix4 matrix4_compose(Matrix4 m1, Matrix4 m2);
+Mat4 matrix4_compose(Mat4 m1, Mat4 m2);
 
 /* given a 4x4 matric and a vector, multiply them together and return
  * the result
  */
-Vector matrix4_vector_multipy(Matrix4 m1, Vector v);
+Vec matrix4_vector_multipy(Mat4 m1, Vec v);
 
-Matrix4 matrix4_transpose(Matrix4 m);
+Mat4 matrix4_transpose(Mat4 m);
 
-Matrix4 matrix4_invert(Matrix4 m);
+Mat4 matrix4_invert(Mat4 m);
 
-float matrix4_determinant(Matrix4 m);
+float matrix4_determinant(Mat4 m);
 
-float matrix3_determinant(Matrix3 m);
+float matrix3_determinant(Mat3 m);
 
-float matrix2_determinant(Matrix2 m);
+float matrix2_determinant(Mat2 m);
 
-Matrix3 matrix4_submatrix(Matrix4 m, int i, int j);
+Mat3 matrix4_submatrix(Mat4 m, int i, int j);
 
-Matrix2 matrix3_submatrix(Matrix3 m, int i, int j);
+Mat2 matrix3_submatrix(Mat3 m, int i, int j);
 
-float matrix4_minor(Matrix4 m, int i, int j);
+float matrix4_minor(Mat4 m, int i, int j);
 
-float matrix3_minor(Matrix3 m, int i, int j);
+float matrix3_minor(Mat3 m, int i, int j);
 
-float matrix4_cofactor(Matrix4 m, int i, int j);
+float matrix4_cofactor(Mat4 m, int i, int j);
 
-float matrix3_cofactor(Matrix3 m, int i, int j);
+float matrix3_cofactor(Mat3 m, int i, int j);
 
 
 #endif /* _MATRIX_H_ */
