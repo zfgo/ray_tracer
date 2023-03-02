ba0aa4a770160a36231c5fe63603c94b511b93fb
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-01 17:21:30 -0800

    initial commit: add tests for matrices

---
+++ src/tests/test_matrix.c
@@ -0,0 +1,51 @@
+#include "tests/test_matrix.h"
+
+void test_matrix4_equal()
+{
+
+}
+
+void test_matrix3_equal()
+{
+
+}
+
+void test_matrix2_equal()
+{
+
+}
+
+void test_matrix4_compose()
+{
+
+}
+
+void test_matrix4_vector_multiply()
+{
+
+}
+
+void test_matrix4_transpose()
+{
+
+}
+
+void test_matrix4_determinant()
+{
+
+}
+
+void test_matrix3_determinant()
+{
+
+}
+
+void test_matrix2_determinant()
+{
+
+}
+
+void test_matrix4_invert()
+{
+
+}
---
+++ src/tests/test_matrix.h
@@ -0,0 +1,20 @@
+#ifndef _TEST_MATRIX_H_
+#define _TEST_MATRIX_H_
+
+#include "datatypes/matrix.h"
+#include "datatypes/vector.h"
+
+// need tests for determinants, inverse, transpose, matrix compose, and matrix-vector multipy
+
+void test_matrix4_equal();
+void test_matrix3_equal();
+void test_matrix2_equal();
+void test_matrix4_compose();
+void test_matrix4_vector_multiply();
+void test_matrix4_transpose();
+void test_matrix4_determinant();
+void test_matrix3_determinant();
+void test_matrix2_determinant();
+void test_matrix4_invert();
+
+#endif /* _TEST_MATRIX_H_ */
