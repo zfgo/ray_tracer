4ab1b1782f1edb38d86ff82aacb9c861517d6d5f
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-01 17:00:55 -0800

    implement determinants and inverting matrices

--- src/datatypes/matrix.c
+++ src/datatypes/matrix.c
@@ -92,12 +92,26 @@ Mat4 matrix4_transpose(Mat4 m)
 
 float matrix4_determinant(Mat4 m)
 {
-    return 0.0;
+    float det = 0.0;
+    int i;
+
+    for (i = 0; i < 4; ++i) {
+        det += m.A[0][i] * matrix4_cofactor(m, 0, i);
+    }
+
+    return det;
 }
 
 float matrix3_determinant(Mat3 m)
 {
-    return 0.0;
+    float det = 0.0;
+    int i;
+
+    for (i = 0; i < 3; ++i) {
+        det += m.A[0][i] * matrix3_cofactor(m, 0, i);
+    }
+
+    return det;
 }
 
 float matrix2_determinant(Mat2 m)
@@ -127,7 +141,7 @@ Mat3 matrix4_submatrix(Mat4 m, int r, int c)
     return res;
 }
 
-Mat2 matrix3_submatrix(Mat3 m, int i, int j)
+Mat2 matrix3_submatrix(Mat3 m, int r, int c)
 {
     Mat2 res;
     int i, j;
@@ -144,6 +158,67 @@ Mat2 matrix3_submatrix(Mat3 m, int i, int j)
             ++out_j;
         }
         ++out_i;
+    }
+
+    return res;
+}
+
+float matrix4_minor(Mat4 m, int i, int j)
+{
+    Mat3 sub;
+    float det;
+
+    sub = matrix4_submatrix(m, i, j);
+    det = matrix3_determinant(sub);
+    
+    return det;
+}
+
+float matrix3_minor(Mat3 m, int i, int j)
+{
+    Mat2 sub;
+    float det;
+
+    sub = matrix3_submatrix(m, i, j); 
+    det = matrix2_determinant(sub);
+
+    return det;
+}
+
+float matrix4_cofactor(Mat4 m, int i, int j)
+{
+    float cofactor;
+
+    cofactor = matrix4_minor(m, i, j);
+
+    return (i + j) % 2 ? -cofactor : cofactor;
+}
+
+float matrix3_cofactor(Mat3 m, int i, int j)
+{
+    float cofactor;
+
+    cofactor = matrix3_minor(m, i, j);
+
+    return (i + j) % 2 ? -cofactor : cofactor;
+}
+
+Mat4 matrix4_invert(Mat4 m)
+{
+    if (matrix4_determinant(m) == 0) {
+        return m;
+    }
+
+    Mat4 res;
+    int i, j;
+    float c;
+    float det = matrix4_determinant(m);
+
+    for (i = 0; i < 4; ++i) {
+        for (j = 0; j < 4; ++j) {
+            c = matrix4_cofactor(m, i, j);
+            res.A[j][i] = c / det;
+        }
     }
 
     return res;
