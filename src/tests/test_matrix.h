17235bd7da58d4ecdee631d63395dd585a263904
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-01 19:07:05 -0800

    removed unnecessary tests

--- src/tests/test_matrix.h
+++ src/tests/test_matrix.h
@@ -13,8 +13,6 @@ void test_matrix4_compose();
 void test_matrix4_vector_multiply();
 void test_matrix4_transpose();
 void test_matrix4_determinant();
-void test_matrix3_determinant();
-void test_matrix2_determinant();
 void test_matrix4_invert();
 
 #endif /* _TEST_MATRIX_H_ */
